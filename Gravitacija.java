4be0f8c4a7793ce016c772203f6b84bab15ec5a0
Drejcc <andrej.fiser1@gmail.com>
2024-03-01 16:15:06 +0100

    Pripravljena funkcija za izpis

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,6 @@
 public class Gravitacija{
     public static void main(String[] args) {
-        System.out.println("OIS je zakon! Se strinjam.");
+        System.out.println("OIS je zakon! Predmet je v redu");
     }
     
     public static double izracunGravitacije(double visina){
@@ -8,5 +8,9 @@ public class Gravitacija{
         double M = 5.972e24;
         double r = 6.371e6;
         return (C * M) / Math.pow(r + visina, 2.0);
+    }
+
+    public static void Izpis(double visina){
+        System.out.printf("Gravitacijski pospešek na %2.f km nadmorske višine je %2.f m/s^2\n", visina, izracunGravitacije(visina * 1000));
     }
 }
